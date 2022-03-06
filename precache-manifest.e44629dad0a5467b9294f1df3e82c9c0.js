c386bd9bb92bf75de0d9a1205f50a157caca9a81
shang <shangg.masood@gmail.com>
2022-03-06 19:42:38 +0300

    Updates

---
+++ precache-manifest.e44629dad0a5467b9294f1df3e82c9c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29cdee5c2dde6e57f775a9f75ca738a",
+    "url": "/PinterestClone/index.html"
+  },
+  {
+    "revision": "f4b9e7388d5fc06f8eae",
+    "url": "/PinterestClone/static/css/main.36c7ce9f.chunk.css"
+  },
+  {
+    "revision": "b19d038ab887267420b1",
+    "url": "/PinterestClone/static/js/2.9ff17c18.chunk.js"
+  },
+  {
+    "revision": "10226f6243d3eddac48fa465895c8df9",
+    "url": "/PinterestClone/static/js/2.9ff17c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac20ce46da38bbe8961",
+    "url": "/PinterestClone/static/js/3.65a36aa1.chunk.js"
+  },
+  {
+    "revision": "f4b9e7388d5fc06f8eae",
+    "url": "/PinterestClone/static/js/main.5de83a80.chunk.js"
+  },
+  {
+    "revision": "77a04afb9f4194ea3df3",
+    "url": "/PinterestClone/static/js/runtime-main.89cae681.js"
+  },
+  {
+    "revision": "717253e4de6621b80555d907c02a7397",
+    "url": "/PinterestClone/static/media/Pinterest-logo.717253e4.png"
+  },
+  {
+    "revision": "06cc6859af85f4d559da28d92f9b956f",
+    "url": "/PinterestClone/static/media/bell-ring.06cc6859.png"
+  },
+  {
+    "revision": "f534d31f409866aa6bae348d929199f5",
+    "url": "/PinterestClone/static/media/chat.f534d31f.png"
+  },
+  {
+    "revision": "c4362de7034822fb6b9347e68968187f",
+    "url": "/PinterestClone/static/media/magnifying-glass.c4362de7.png"
+  },
+  {
+    "revision": "0d713ad3f2354fa98c4051065facfcc8",
+    "url": "/PinterestClone/static/media/user.0d713ad3.png"
+  }
+]);
